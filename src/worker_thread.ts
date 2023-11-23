2f9a8f06d9caac42a34dbc86be443483ada1eac4
Evmos <longdieu12x@gmail.com>
2023-11-23 12:38:50 +0700

    update case error for worker thread

--- src/worker_thread.ts
+++ src/worker_thread.ts
@@ -1,16 +1,18 @@
 import { Worker } from "node:worker_threads";
 import { WorkerThreadTypes } from "worker_thread.interface";
+import { ethers } from "ethers";
 
 export async function sampleWorkerThread(workerType: WorkerThreadTypes) {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "https://ethereum-goerli.publicnode.com"
+  );
+
   const worker = new Worker(`${workerType}`, {
     workerData: {
-      value: 10000000000,
+      value: 1000000000,
+      //   provider, // PANIC here: because can only pass pure variable
     },
   });
-
-  setInterval(() => {
-    console.log("Tuhu...");
-  }, 1000);
 
   worker.on("message", (data) => {
     console.log("Test data: ", data);
--- src/workers/count.ts
+++ src/workers/count.ts
@@ -1,12 +1,18 @@
+import { ethers } from "ethers";
 import { parentPort, workerData } from "worker_threads";
 import { CountResponse } from "workers/count.interface";
 
-let initData = workerData.value as number;
-let total = 0;
-for (let i = 0; i < initData; i++) {
-  total++;
-}
+const handleTotal = async () => {
+  let initData = workerData.value as number;
+  let total = 0;
+  for (let i = 0; i < initData; i++) {
+    total++;
+  }
+  return total;
+};
 
-parentPort.postMessage({
-  data: total,
-} as CountResponse);
+handleTotal().then((res) => {
+  parentPort.postMessage({
+    data: res,
+  } as CountResponse);
+});
