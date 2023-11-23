8d217adcc920fb2bb68b78368282ab2e70c3887d
Evmos <longdieu12x@gmail.com>
2023-11-23 12:40:08 +0700

    clean esbuild

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,7 +2,7 @@ const esbuild = require("esbuild");
 
 esbuild
   .build({
-    entryPoints: ["src/index.ts", "src/workers/count.ts"],
+    entryPoints: ["src/index.ts", "src/workers/*.ts"],
     bundle: true,
     outdir: "dist",
     format: "cjs",
